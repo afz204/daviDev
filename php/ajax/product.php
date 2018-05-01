120cfb5ededb87f697bfb0821af7024c06c55caf
afz204 <afz60.30@gmail.com>
2018-05-02 03:42:02 +0700

    products

---
+++ php/ajax/product.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: small-project
+ * Date: 01/05/2018
+ * Time: 19.24
+ */
---
+++ php/bd/product.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: small-project
+ * Date: 02/05/2018
+ * Time: 00.57
+ */
